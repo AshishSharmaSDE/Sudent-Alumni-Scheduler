6882ca47481c19242c9c995b0e37965b0f45d438
Ashish Sharma <Ashuparashar95@gmail.com>
2019-05-24 13:51:41 +0530

    try again

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('/users/register', function(err){
+mongoose.connect('mongodb+srv://Ashish:<1234>@student-alumni-scheduler-k1koc.mongodb.net/test?retryWrites=true', function(err){
   if(err){
     console.error('MongoDB connection error: ' + JSON.stringify(err, undefined, 2));
   }
