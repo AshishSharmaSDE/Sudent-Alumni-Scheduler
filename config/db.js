3f0fc879620712d3a1c19172c50c601b64d03abb
Ashish Sharma <Ashuparashar95@gmail.com>
2019-05-24 07:59:13 +0530

    ready for use

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/snippt-scheduler', function(err){
+mongoose.connect('mongodb://localhost/student-alumni-scheduler', function(err){
   if(err){
     console.error('MongoDB connection error: ' + JSON.stringify(err, undefined, 2));
   }
