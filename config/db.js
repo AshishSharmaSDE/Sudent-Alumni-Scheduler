29f03c37da05d8e0f6971325150264493b06ef57
Ashish Sharma <Ashuparashar95@gmail.com>
2019-05-24 14:23:13 +0530

    updated again

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
+const uri ="mongodb+srv://Ashish:<1234>@cluster0-k1koc.mongodb.net/test?retryWrites=true";
 
-mongoose.connect('mongodb+srv://Ashish:<1234>@cluster0-k1koc.mongodb.net/test?retryWrites=true', function(err){
+mongoose.connect(uri, function(err){
   if(err){
     console.error('MongoDB connection error: ' + JSON.stringify(err, undefined, 2));
   }
